f808121812ee59514acc87b08e195f224ae7537b
loicchen <910041@gmail.com>
2026-07-03 04:25:54 +0800

    chore: change VoxelWorld example camera facing

--- Examples/VoxelWorld/main.cpp
+++ Examples/VoxelWorld/main.cpp
@@ -26,7 +26,8 @@ class VoxelWorldApp : public Application {
     void OnLoad() override {
 
         mainCamera->gameObject->SetPosition(glm::vec3(200.0f, 80.0f, 200.0f));
-        mainCamera->gameObject->SetRotation(glm::vec3(glm::radians(-20.0f), 0.0f, 0.0f));
+        mainCamera->Pitch(glm::radians(-20.0f));
+        mainCamera->Yaw(glm::radians(-75.0f));
         mainCamera->gameObject->AddComponent<FlyCameraComponent>(/*moveSpeed=*/20.0f, /*lookSpeed=*/1.5f);
 
         auto* worldObj = CreateGameObject();
